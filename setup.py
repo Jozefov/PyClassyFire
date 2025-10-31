dd512a0f81f85fd280b96c6c5d415d537be39d61
Jozefov <mn.mastaf@gmail.com>
2025-10-31 13:28:19 +0100

    add ipykernel:

--- setup.py
+++ setup.py
@@ -6,36 +6,37 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='PyClassyFire',
-    version='0.1.0',
-    author='Filip Jozefov',
-    author_email='your.email@example.com',
-    description='A Python client for the ClassyFire API for large-scale chemical compound classification.',
+    name="PyClassyFire",
+    version="0.1.0",
+    author="Filip Jozefov",
+    author_email="your.email@example.com",
+    description="A Python client for the ClassyFire API for large-scale chemical compound classification.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url='https://github.com/Jozefov/PyClassyFire',
+    url="https://github.com/Jozefov/PyClassyFire",
     packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Chemistry',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Chemistry",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-        'requests>=2.32.3',
-        'click>=8.1.7',
-        'tqdm>=4.66.5',
-        'rdkit>=2024.3.5',
-        'pandas>=2.2.3',
+        "requests>=2.32.3",
+        "click>=8.1.7",
+        "tqdm>=4.66.5",
+        "rdkit>=2024.3.5",
+        "pandas>=2.2.3",
+        "ipykernel>=6.29",
     ],
     entry_points={
-        'console_scripts': [
-            'pyclassyfire=pyclassyfire.cli:cli',
+        "console_scripts": [
+            "pyclassyfire=pyclassyfire.cli:cli",
         ],
     },
     project_urls={
-        'Source': 'https://github.com/Jozefov/PyClassyFire',
+        "Source": "https://github.com/Jozefov/PyClassyFire",
     },
 )
